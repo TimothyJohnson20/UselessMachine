dcdb4cd9142c9f1b6effed345569f02295ad4dc9
Timothy Johnson <timothyjohnsontimothy@gmail.com>
2019-08-28 13:58:20 -0700

    Finished switch, started button

--- app/src/main/java/com/example/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/uselessmachine/MainActivity.java
@@ -3,12 +3,78 @@ package com.example.uselessmachine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button SELF_DESTRUCT;
+    private Switch useless;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        wireWidgets();
+        setListeners();
+
+    }
+    private void wireWidgets() {
+        SELF_DESTRUCT = findViewById(R.id.button_main_self_destruct);
+        useless = findViewById(R.id.switch_main_useless);
+    }
+    private void setListeners() {
+        SELF_DESTRUCT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new CountDownTimer(5000, 1000) {
+                    @Override
+                    public void onTick(long l) {
+                        Toast.makeText(MainActivity.this, "...", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        System.exit(0);
+                    }
+                }
+
+            }
+        });
+        useless.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    new CountDownTimer(2000, 10) {
+
+                        @Override
+                        public void onTick(long l) {
+                            if(!useless.isChecked()){
+                                cancel();
+                            }
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            useless.setChecked(false);
+                        }
+                    }.start();
+                }
+
+
+//                if(b){
+//                    Toast.makeText(MainActivity.this, "On", Toast.LENGTH_SHORT).show();
+//                }
+//                else{
+//                    Toast.makeText(MainActivity.this, "Off", Toast.LENGTH_SHORT).show();
+//
+//                }
+            }
+        });
     }
 }
